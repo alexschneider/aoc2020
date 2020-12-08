f9def0ac7886b52d697197030c7f035ad69a6d25
Alex Schneider <alex@schneids.me>
2020-12-07 23:27:35 -0800

    Faster!

--- src/day8.rs
+++ src/day8.rs
@@ -1,29 +1,41 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 use std::collections::HashSet;
 
+#[derive(Clone)]
+enum Instruction {
+    Acc(i32),
+    Jmp(i32),
+    Nop(i32),
+}
+
 #[aoc_generator(day8)]
-fn program(input: &str) -> Vec<(&str, i32)> {
+fn program(input: &str) -> Vec<Instruction> {
   input.lines().map(|x| {
     let spl: Vec<&str> = x.split(' ').collect();
-    (spl[0], spl[1].parse::<i32>().unwrap())
+    let num = spl[1].parse::<i32>().unwrap();
+    match spl[0] {
+      "acc" => Instruction::Acc(num),
+      "jmp" => Instruction::Jmp(num),
+      "nop" => Instruction::Nop(num),
+      _ => unreachable!()
+    }
   }).collect()
 }
 
 #[aoc(day8, part1)]
-fn part1(program: &[(&str, i32)]) -> String {
-  let mut visited = HashSet::new();
+fn part1(program: &[Instruction]) -> String {
+  let mut visited = vec![false; program.len()];
   let mut acc: i32 = 0;
   let mut op_index: i32 = 0;
   loop {
-    visited.insert(op_index);
-    let (op, num) = program[op_index as usize];
-    match op {
-      "nop" => op_index += 1,
-      "acc" => {op_index += 1; acc += num as i32},
-      "jmp" => op_index += num,
+    visited[op_index as usize] = true;
+    match program[op_index as usize] {
+      Instruction::Nop(_) => op_index += 1,
+      Instruction::Acc(num) => {op_index += 1; acc += num as i32},
+      Instruction::Jmp(num) => op_index += num,
       _ => unreachable!()
     }
-    if visited.contains(&op_index) {
+    if visited[op_index as usize] {
       break;
     }
   }
@@ -31,26 +43,25 @@ fn part1(program: &[(&str, i32)]) -> String {
 }
 
 #[aoc(day8, part2)]
-fn part2(program: &[(&str, i32)]) -> String {
+fn part2(program: &[Instruction]) -> String {
   
 
-  let mut visited = HashSet::new();
+  let mut visited = vec![false; program.len()];
   let mut acc: i32 = 0;
   let mut op_index: i32 = 0;
   let mut state = Vec::new();
   let mut brute_force = false;
   loop {
-    visited.insert(op_index);
-    let (op, num) = program[op_index as usize];
-    match op {
-      "nop" => {
+    visited[op_index as usize] = true;
+    match program[op_index as usize] {
+      Instruction::Nop(_) => {
         if !brute_force {
           state.push((op_index, acc, visited.clone())); 
         }
         op_index += 1; 
       },
-      "acc" => {op_index += 1; acc += num as i32},
-      "jmp" => {
+      Instruction::Acc(num) => {op_index += 1; acc += num as i32},
+      Instruction::Jmp(num) => {
         if !brute_force {
           state.push((op_index, acc, visited.clone())); 
         }
@@ -58,21 +69,20 @@ fn part2(program: &[(&str, i32)]) -> String {
       },
       _ => unreachable!()
     }
-    if visited.contains(&op_index) {
+    if op_index as usize >= program.len() {
+      break;
+    }
+    if visited[op_index as usize] {
       let (index, accum, visit) = state.pop().unwrap();
       op_index = index;
       acc = accum;
       visited = visit;
-      let (o, n) = program[index as usize];
-      match o {
-        "jmp" => {op_index += 1},
-        "nop" => {op_index += n},
+      match program[index as usize] {
+        Instruction::Jmp(_) => {op_index += 1},
+        Instruction::Nop(n) => {op_index += n},
         _ => unreachable!()
       }
       brute_force = true;
-    }
-    if op_index as usize >= program.len() {
-      break;
     }
   }
 
